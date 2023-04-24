cc6d239ca6b21f539e1b9c2bf38e0f7a756c875d
preloboy <72853954+preloboy@users.noreply.github.com>
2023-04-24 18:10:11 +0530

    two way binding

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,37 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { findIndex } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   template: `
+    <div class="container p-3">
     <h1>Hello from {{name}}!</h1>
-    <input type='text' placeholder='Type here ...' #task /> <br/><br/>
-    <button (click)="addTask(task.value)">Add</button><br><br>
-    <ol *ngFor="let task of tasks">
-    <li>{{task}} <button (click)="removeTask(task)">Remove</button></li>
-    </ol>
+    <div class="d-flex">
+    <input class="form-control" type='text' placeholder='Type here ...' #task [(ngModel)]="preview"/> 
+    <button (click)="addTask(task.value)" class="btn btn-primary mb-3 mx-1">Add</button>
+    </div>
+    <label class="form-label px-2 mx-1">{{preview}}</label>
+    <ul *ngFor="let task of tasks" class="list-group d-flex justify-content-between">
+    <li class="list-group-item">{{task}}<button class="btn btn-primary m-0" (click)="removeTask(task)">Remove</button></li>
+    </ul>
+    </div>
   `,
 })
 export class App {
+  preview: any;
   name = 'Angular';
   color = 'ff';
   display = false;
   tasks = [];
   addTask(item) {
     // console.warn(item);
-    this.tasks.includes(item) || item==="" ? alert("Already added or Nothing to add !") : this.tasks.push(item);
+    this.tasks.includes(item) || item === ''
+      ? alert('Already added or Nothing to add !')
+      : this.tasks.push(item);
     console.log(this.tasks);
   }
   removeTask(item) {
