1caee0300ca72803ddcf124a03311e01fe9599d2
preloboy <72853954+preloboy@users.noreply.github.com>
2023-04-25 12:24:42 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -9,36 +9,39 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   template: `
-    <div class="container p-3">
-    <h1>Hello from {{name}}!</h1>
-    <div class="d-flex">
-    <input class="form-control" type='text' placeholder='Type here ...' #task [(ngModel)]="preview"/> 
-    <button (click)="addTask(task.value)" class="btn btn-primary mb-3 mx-1">Add</button>
+    <div class="container my-3"> 
+      <label for="exampleFormControlTextarea1" class="form-label"> <h1>{{name}}</h1></label>
+      <textarea #task [(ngModel)]="preview" class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Type here ..."></textarea>
     </div>
-    <label class="form-label px-2 mx-1">{{preview}}</label>
-    <ul *ngFor="let task of tasks" class="list-group d-flex justify-content-between">
-    <li class="list-group-item">{{task}}<button class="btn btn-primary m-0" (click)="removeTask(task)">Remove</button></li>
-    </ul>
+    <div class="container">
+      <button (click)="addTask(task.value)" class="btn btn-primary mb-3 mx-1">Add</button>
+    </div>
+    <div class="container">
+      <ul *ngFor="let task of tasks" class="list-group d-flex">
+        <li class="list-group-item d-flex justify-content-between align-items-center">{{task}}
+        <button class="btn btn-primary m-0 py-0" (click)="removeTask(task)">Remove</button>
+        </li>
+      </ul>
     </div>
   `,
 })
 export class App {
-  preview: any;
-  name = 'Angular';
+  preview: string;
+  name = 'To-Do';
   color = 'ff';
   display = false;
-  tasks = [];
+  tasks = ['To-do'];
   addTask(item) {
-    // console.warn(item);
+    console.warn(item);
     this.tasks.includes(item) || item === ''
       ? alert('Already added or Nothing to add !')
       : this.tasks.push(item);
-    console.log(this.tasks);
+    // console.log(this.tasks);
   }
   removeTask(item) {
     let index = this.tasks.indexOf(item);
     console.warn(index);
-    this.tasks.splice(index);
+    this.tasks.splice(index,1);
   }
 }
 
